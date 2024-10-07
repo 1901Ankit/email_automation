8897c87819d732fe78b04e5d41ab4e306d845dec
Gokul Jana <125233243+janagokul6@users.noreply.github.com>
2024-10-08 05:09:14 +0530

    new saved changes

--- src/pages/Preview/index.js
+++ src/pages/Preview/index.js
@@ -24,8 +24,9 @@ const Preview = ({ placeholder }) => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+  const [emailStatus, setEmailStatus] = useState(null);
   const [content, setContent] = useState(``);
-  const [imageURL, setImageURL] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
   const [HTMLtemplate, setHTMLtemplate] = useState(null)
   const [details, setDetails] = useState({});
   const [options, setOptions] = useState({ smtps: [] });
@@ -60,29 +61,6 @@ const Preview = ({ placeholder }) => {
         );
         const html = await response.text();
         setHTMLtemplate(html)
-
-        // const tempDiv = document.createElement("div");
-        // tempDiv.innerHTML = html;
-        // document.body.appendChild(tempDiv);
-        // // Wait for all images to load
-        // const images = tempDiv.getElementsByTagName("img");
-        // const imageLoadPromises = Array.from(images).map((img) => {
-        //   return new Promise((resolve, reject) => {
-        //     img.onload = resolve;
-        //     img.onerror = () => {
-        //       console.error("Image failed to load:", img.src);
-        //       resolve(); // Resolve even on error to continue
-        //     };
-        //   });
-        // });
-
-        // // Wait for all images to load
-        // await Promise.all(imageLoadPromises);
-        // await new Promise((resolve) => setTimeout(resolve, 100));
-        // const canvas = await html2canvas(tempDiv, { useCORS: true });
-        // const selectedHTMLImageURL = canvas.toDataURL();
-        // setImageURL(selectedHTMLImageURL);
-        // document.body.removeChild(tempDiv);
       } catch (error) {
         console.log(error);
       }
@@ -114,6 +92,7 @@ const Preview = ({ placeholder }) => {
     closeModal();
   };
   const handleSendEmail = async () => {
+    setIsLoading(true)
     const formData = new FormData();
 
     options?.smtps?.forEach((element) => {
@@ -130,6 +109,7 @@ const Preview = ({ placeholder }) => {
 
     try {
       const response = await sendEmailAPI.sendEmail(formData);
+      setEmailStatus(response.data)
       toast.success("Email sent successfully")
       console.log("Emails sent successfully:", response.data);
 
@@ -139,6 +119,7 @@ const Preview = ({ placeholder }) => {
         error.response?.data || error.message
       );
     }
+    setIsLoading(false)
   };
 
   const handleEditorChange = (state) => {
@@ -233,52 +214,32 @@ const Preview = ({ placeholder }) => {
                             className="w-full h-full"
                           />
                         </div>
-                        {/* <iframe
-                          src={`https://emailbulkshoot.s3.amazonaws.com/${JSON.parse(sessionStorage.getItem('key'))}`}
-                          // alt="Selected"
-                          height={"100%"} width={"100%"}
-                        /> */}
-                        {/* <img src={imageURL} alt="" /> */}
                       </div>
                     </div>
                     <div className="button-container mt-3">
                       <button
+                        disabled={isLoading}
                         onClick={handleSendEmail}
                         type="button"
                         className="preview-button"
                       >
-                        SEND MESSAGE NOW
+                        {isLoading ? (
+                          <div role="status">
+                            <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                            </svg>
+                            <span class="sr-only">Loading...</span>
+                          </div>
+                        ) : "SEND MESSAGE NOW"}
                       </button>
                     </div>
                   </div>
                 </div>
               </div>
-
-              {/* {isModalOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-                  <div className="bg-white p-6 rounded-lg w-full max-w-[50%] max-h-[90vh] overflow-auto">
-                    <h2 className="text-xl font-bold mb-4">Edit Content</h2>
-                   
-                    <div className="flex justify-end mt-4">
-                      <button
-                        onClick={closeModal}
-                        className="mr-2 bg-gray-300 px-4 py-2 rounded-md"
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        onClick={handleSave}
-                        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                      >
-                        Save
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              )} */}
             </form>
             <div className="w-[30%]">
-              <Rightside />
+              <Rightside emailStatus={emailStatus} />
             </div>
           </div>
         )}
